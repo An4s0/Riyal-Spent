b6f9bc18c01de068ca71fa2af5a43f126eb8b0c0
Anas Almutary <00063q@gmail.com>
2025-11-25 16:48:03 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/', require('./routes'));
+
+const port = 3005;
+app.listen(port, () => {
+  console.log(`   \x1b[32m➜\x1b[0m  Local:   \x1b[36mhttp://localhost:${port}\x1b[0m`);
+});
