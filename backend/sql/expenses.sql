622f4cee0bdb0a9d262cd795af870061278a8496
Anas Almutary <00063q@gmail.com>
2025-11-26 16:08:33 +0300

    add expenses table

---
+++ backend/sql/expenses.sql
@@ -0,0 +1,18 @@
+CREATE TABLE
+    expenses (
+        -- Primary key
+        expense_id INT AUTO_INCREMENT PRIMARY KEY,
+        -- Relations
+        user_id INT NOT NULL,
+        category_id INT,
+        -- Main data
+        amount DECIMAL(10, 2) NOT NULL,
+        date DATE NOT NULL,
+        description VARCHAR(255),
+        -- Timestamps
+        created_at DATETIME,
+        last_modified DATETIME,
+        -- Foreign keys
+        FOREIGN KEY (user_id) REFERENCES user (user_id),
+        FOREIGN KEY (category_id) REFERENCES category (category_id)
+    );
