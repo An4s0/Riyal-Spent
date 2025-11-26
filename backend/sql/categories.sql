44eea9e112450056f7fc4faf03c52c54dabafcb4
Anas Almutary <00063q@gmail.com>
2025-11-26 16:19:12 +0300

    add categories table

---
+++ backend/sql/categories.sql
@@ -0,0 +1,15 @@
+CREATE TABLE
+    categories (
+        -- Primary key
+        category_id INT AUTO_INCREMENT PRIMARY KEY,
+        -- Relation
+        user_id INT NOT NULL,
+        -- Category info
+        name VARCHAR(100) NOT NULL,
+        icon VARCHAR(50),
+        color_code VARCHAR(7), 
+        -- Timestamp
+        created_at DATE,
+        -- Foreign key
+        FOREIGN KEY (user_id) REFERENCES user (user_id)
+    );
